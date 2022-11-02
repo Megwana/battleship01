38e37dd5409bb68626e935763ae7305d090e8990
Megan <megantech@hotmail.com>
2022-11-02 14:53:16 +0000

    create board

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from random import randint
 
-# create hidden pattern and guess pattern with a rate of 5
+# create hidden pattern and guess pattern with a range of 5
 Hidden_Pattern=[[' ']*5 for x in range(5)]
 Guess_Pattern=[[' ']*5 for x in range(5)]
+
+
+# create battleship board
+def print_board(board):
+    print('# A B C D E')
+    row_num=1
+    for row in board:
+        print("%d|%s|" % (row_num, "|".join(row)))
+        row_num +=1
