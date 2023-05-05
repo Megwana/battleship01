d5069bf286972ac98652ec754b1d7f27958cad2e
Megan <megantech@hotmail.com>
2023-05-05 10:24:27 +0000

    Improved take_name_input with exeption/error handling for numbers and catching too general exception in 'problem' terminal

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ Imported randint from random
 from random import randint
 
 
-HIDDEN_PATTERN = [[' ']*5 for x in range(5)]
-GUESS_PATTERN = [[' ']*5 for x in range(5)]
+HIDDEN_PATTERN = [[' '] * 5 for x in range(5)]
+GUESS_PATTERN = [[' '] * 5 for x in range(5)]
 """Create hidden pattern and guess pattern with a range of 5"""
 
 # Constants that are used to represent what is on the board
@@ -16,16 +16,17 @@ alph_digit = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
 
 
 def take_name_input():
-    """Makes sure players name is 1 character or more"""
-    try:
-        user_name = input("Please enter your name: \n")
-        if not user_name:
-            raise Exception("Your First name must be greater than"
-            + " 1 character")
-        return user_name
-    except Exception as e:
-        print(e)
-        return take_name_input()
+    """Makes sure player's name is 1 character or more"""
+    while True:
+        try:
+            user_name = input("Please enter your name: \n")
+            if not user_name:
+                raise ValueError("Your first name must be greater than 1 character")
+            if any(char.isdigit() for char in user_name):
+                raise ValueError("Your first name must contain only letters")
+            return user_name
+        except ValueError as e:
+            print(e)
 
 
 def welcome():
@@ -68,17 +69,17 @@ def generate_ships():
             row = int(row) - 1
             break
         else:
-            print("Invalid input. Please enter a number between 1 and 5.")
+            print(
+                "Invalid input. Please enter a number between 1 and 5.")
     while True:
         column = input("Enter column (A-E): ")
         if column.isalpha() and len(column) == 1 and 'A' <= column.upper() <= 'E':
             column = alph_digit[column.upper()]
             break
         else:
-            print("Invalid input. Please enter a letter between A and E.")
+            print(
+                "Invalid input. Please enter a letter between A and E.")
     return row, column
-
-
 
 
 def random_ships(board):
@@ -86,7 +87,7 @@ def random_ships(board):
     function creates random ship locations on board.
     Using the randint imported at the begining.
     """
-    for ship in range(5):
+    for random_ships in range(5):
         ship_row, ship_col = randint(0, 4), randint(0, 4)
         while board[ship_row][ship_col] == HIT_SYMBOL:
             ship_row, ship_col = randint(0, 4), randint(0, 4)
@@ -137,11 +138,11 @@ def main():
         \n All enemy ships have been hit.""")
         exit()
     while turns == 0:
-        play_input = input("Do you want to play again? (Y/N): ").strip().lower()
-        if play_input == 'y':
+        main = input("Do you want to play again? (Y/N): ").strip().lower()
+        if main == 'y':
             main()
             break
-        elif play_input == 'n':
+        elif main == 'n':
             print("Goodbye for now!")
             quit()
         else:
